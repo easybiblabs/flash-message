94dfb7fb262b1efb27d1780480ff4249a3c487d4
sashless <saweniger@gmail.com>
2015-01-26 17:18:53 +0100

    fix missing angular-translate dependency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 var angular = require('angular-bsfy');
+require('angular-translate');
 
-angular.module('flash-message', [])
+angular.module('flash-message', [
+	'pascalprecht.translate'
+])
 	.directive('flashMessage', require('./flash-directive'))
 	.factory('FlashMessage', require('./flash-factory'));
