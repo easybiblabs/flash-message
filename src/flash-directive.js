adfbaf1344d00ebec89653fd87ca2421fdd4ad29
sashless <saweniger@gmail.com>
2015-01-26 16:29:51 +0100

    flash-directive to browserify

--- src/flash-directive.js
+++ src/flash-directive.js
@@ -1,27 +1,22 @@
-define(function() {
+module.exports = function FlashDirective() {
   'use strict';
+  var directive = {
+    restrict: 'E',
+    replace: true,
+    templateUrl: '/partials/lib/message/flash.html',
+    controller: ['$scope', '$rootScope', '$timeout', function($scope, $rootScope, $timeout) {
+      $rootScope.$on('flash:message', function(_, messages) {
+        $scope.messages = messages;
+        $scope.visible = true;
+        $timeout(function() {
+          $scope.visible = false;
+        }, 3000);
+        $timeout(function() {
+          $scope.messages = [];
+        }, 4000);
+      });
+    }]
+  };
 
-  function FlashDirective() {
-    var directive = {
-      restrict: 'E',
-      replace: true,
-      templateUrl: '/partials/lib/message/flash.html',
-      controller: ['$scope', '$rootScope', '$timeout', function($scope, $rootScope, $timeout) {
-        $rootScope.$on('flash:message', function(_, messages) {
-          $scope.messages = messages;
-          $scope.visible = true;
-          $timeout(function() {
-            $scope.visible = false;
-          }, 3000);
-          $timeout(function() {
-            $scope.messages = [];
-          }, 4000);
-        });
-      }]
-    };
-
-    return directive;
-  }
-
-  return FlashDirective;
-});
+  return directive;
+};
--- src/flash-directive_spec.js
+++ src/flash-directive_spec.js
@@ -1,56 +1,67 @@
-define(['app/lib/message/flash-directive', 'angular-mocks'], function(flashDirective) {
+require('./index');
+require('angular-mocks');
+
+var flashDirective = require('./flash-directive');
+
+var chai = require('chai');
+chai.should();
+
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+
+describe('Directive: flashMessages', function() {
   'use strict';
-  describe('Directive: flashMessages', function() {
-    describe('#controller', function() {
-      var $timeout,
-        $rootScope,
-        $scope,
-        directive = flashDirective(),
-        onSpy;
 
-      function setup() {
-        inject(function(_$rootScope_, _$timeout_) {
-          $timeout = _$timeout_;
-          $rootScope = _$rootScope_;
-          $scope = $rootScope.$new();
-          if (!onSpy) {
-            onSpy = sinon.spy($rootScope, '$on');
-          }
-          directive.controller[3]($scope, $rootScope, $timeout);
-        });
-      }
+  describe('#controller', function() {
+    var $timeout,
+      $rootScope,
+      $scope,
+      directive = flashDirective(),
+      onSpy;
 
-      beforeEach(setup);
+    function setup() {
+      inject(function(_$rootScope_, _$timeout_) {
+        $timeout = _$timeout_;
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
+        if (!onSpy) {
+          onSpy = sinon.spy($rootScope, '$on');
+        }
+        directive.controller[3]($scope, $rootScope, $timeout);
+      });
+    }
 
-      it('adds a listener to rootScope', function() {
-        onSpy.should.have.been.calledWith('flash:message', sinon.match.func);
+    beforeEach(setup);
+
+    it('adds a listener to rootScope', function() {
+      onSpy.should.have.been.calledWith('flash:message', sinon.match.func);
+    });
+
+    describe('listener', function() {
+      it('sets messages on scope', function() {
+        var messages = ['booyah'];
+        $scope.$emit('flash:message', messages);
+        $scope.messages.should.equal(messages);
+        $timeout.flush();
       });
 
-      describe('listener', function() {
-        it('sets messages on scope', function() {
-          var messages = ['booyah'];
-          $scope.$emit('flash:message', messages);
-          $scope.messages.should.equal(messages);
-          $timeout.flush();
-        });
+      it('sets visible to true', function() {
+        $scope.$emit('flash:message', []);
+        $scope.visible.should.equal(true);
+        $timeout.flush();
+      });
 
-        it('sets visible to true', function() {
-          $scope.$emit('flash:message', []);
-          $scope.visible.should.equal(true);
-          $timeout.flush();
-        });
+      it('waits 3 seconds, then turns off visible', function() {
+        $scope.$emit('flash:message', []);
+        $timeout.flush(3000);
+        $scope.visible.should.equal(false);
+      });
 
-        it('waits 3 seconds, then turns off visible', function() {
-          $scope.$emit('flash:message', []);
-          $timeout.flush(3000);
-          $scope.visible.should.equal(false);
-        });
-
-        it('waits 4 seconds, then empties the messages array', function() {
-          $scope.$emit('flash:message', []);
-          $timeout.flush(4000);
-          $scope.messages.length.should.equal(0);
-        });
+      it('waits 4 seconds, then empties the messages array', function() {
+        $scope.$emit('flash:message', []);
+        $timeout.flush(4000);
+        $scope.messages.length.should.equal(0);
       });
     });
   });
