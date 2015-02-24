f6958d5cc2998972d90f044580f5e1300e4d5100
sashless <saweniger@gmail.com>
2015-02-24 23:38:48 +0100

    try to fix the bundle

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,12 @@
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.flashMessage=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 require('angular');
 require('angular-translate');
 
-module.exports = (function() {
-  'use strict';
-
-  angular.module('flash-message', [
-    'pascalprecht.translate'
-  ])
-    .directive('flashMessage', require('modules/flash-directive'))
-    .factory('FlashMessage', require('modules/flash-factory'));
-})();
+angular.module('flash-message', [
+	'pascalprecht.translate'
+])
+	.directive('flashMessage', require('modules/flash-directive'))
+	.factory('FlashMessage', require('modules/flash-factory'));
 
 },{"angular":2,"angular-translate":2,"modules/flash-directive":3,"modules/flash-factory":4}],2:[function(require,module,exports){
 
@@ -119,5 +115,4 @@ module.exports = (function() {
   return FlashService;
 }());
 
-},{}]},{},[1])(1)
-});
+},{}]},{},[1]);
